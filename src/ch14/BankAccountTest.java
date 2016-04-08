86ddb631fe41a84e9a136d17d2f0f254d3c023b0
Debashis Biswas <S17400233@stu.tuhsd.adn>
2016-04-08 11:29:55 -0700

    finished activities pt 1

--- src/ch14/BankAccountTest.java
+++ src/ch14/BankAccountTest.java
@@ -1,5 +1,6 @@
 package ch14;
 
+import java.text.NumberFormat;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,12 +9,38 @@ import javax.swing.JOptionPane;
 public class BankAccountTest {
 	public static void main(String[] args) {
 		boolean done = false;
-		do{
-			try{
-				BankAccount account = new BankAccount(Double.parseDouble(JOptionPane.showInputDialog(null, "Enter a starting value: ", "Bank Account", JOptionPane.QUESTION_MESSAGE)));	
+		BankAccount account = null;
+		do {
+			try {
+				account = new BankAccount(Double.parseDouble(JOptionPane.showInputDialog(null,
+						"Enter a starting value: ", "Bank Account", JOptionPane.QUESTION_MESSAGE)));
 				done = true;
-			} catch (Exception e){
-				
+			} catch (Exception e) {
+
+			}
+		} while (!done);
+
+		done = false;
+
+		do {
+			try {
+				account.deposit(Double.parseDouble(JOptionPane.showInputDialog(null, "Enter a deposit amount: ",
+						"Current Balance: " + NumberFormat.getCurrencyInstance().format(account.getBalance()),
+						JOptionPane.QUESTION_MESSAGE)));
+				done = true;
+			} catch (Exception e) {
+
+			}
+		} while (!done);
+
+		do {
+			try {
+				account.withdraw(Double.parseDouble(JOptionPane.showInputDialog(null, "Enter a withdrawal amount: ",
+						"Current Balance: " + NumberFormat.getCurrencyInstance().format(account.getBalance()),
+						JOptionPane.QUESTION_MESSAGE)));
+				done = true;
+			} catch (Exception e) {
+
 			}
 		} while (!done);
 	}
