e1f889ff9335cfc96a09804204a85a1f1151b0ac
Debashis Biswas <S17400233@stu.tuhsd.adn>
2016-04-12 11:32:33 -0700

    needs some organization

--- src/ch14/AppointmentBookTest.java
+++ src/ch14/AppointmentBookTest.java
@@ -1,9 +1,15 @@
 package ch14;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.text.ParseException;
+import java.util.Scanner;
 
+import javax.swing.JFileChooser;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class AppointmentBookTest {
 	public static void main(String[] args) {
@@ -14,6 +20,32 @@ public class AppointmentBookTest {
 			e.printStackTrace();
 		}
 		
-		//add that user input for the data file
+		AppointmentBook myBook = new AppointmentBook();
+
+		FileReader fr = null;
+		Scanner scanner = null;
+		try {
+			JFileChooser fc = new JFileChooser("H:/workspace/ch14");
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files", "txt", "text");
+			fc.setFileFilter(filter);
+			fc.showOpenDialog(null);
+			Path filePath = fc.getSelectedFile().toPath();
+			
+			
+			scanner = new Scanner(filePath);
+			
+			while(scanner.hasNextLine()){
+				myBook.add(scanner.nextLine(), scanner.nextLine());
+				//not secure
+			}
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fr.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
